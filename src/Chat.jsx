b52256bf138fa7833f5398a5a9eeaf88e733286f
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-08-20 11:53:46 +0545

    -added react and css styles

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -4,14 +4,25 @@ import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from '@material-ui/icons/Mic';
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { AttachFile, SearchOutlined } from "@material-ui/icons";
+
 function Chat(props) {
+
   const [seed, setSeed] = useState("");
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  const sendMessage=(e)=>{
+    e.preventDefault();
+    console.log("you typed >>>",input);
+    setInput("");
+  }
 
   return (
     <div className="chat">
@@ -34,10 +45,20 @@ function Chat(props) {
         </div>
       </div>
       <div className="chat__body">
-        <p className="chat__message">Hey Guys</p>
-        <p className="chat__message">Hey Guys</p>
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">Sagar Siwakoti</span>
+          Hey Guys
+          <span className="chat__timestamp">3:52pm</span>
+        </p>
       </div>
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input type="text" placeholder="Type a message..." value={input} onChange={e=>setInput(e.target.value)}/>
+          <button type="submit" onClick={sendMessage}>Send a message</button>
+        </form>
+        <MicIcon/>
+      </div>
     </div>
   );
 }
